d517be20ebb620f32c4056ff4e65ecc092c4057b
Hoang Vu <vuhse182692@fpt.edu.vn>
2026-03-26 11:48:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+async function updateRSS() {
+    const CHANNEL_ID = "UCeHc6r9TtpQaRTUDL-WRcMw";
+    const YT_URL = `https://www.youtube.com/feeds/videos.xml?channel_id=${CHANNEL_ID}`;
+
+    try {
+        const response = await fetch(YT_URL, {
+            headers: { "User-Agent": "Mozilla/5.0" }
+        });
+        const fullXml = await response.text();
+
+        fs.writeFileSync('rss.xml', fullXml);
+        console.log("✅ Đã lọc và tạo file rss.xml thành công!");
+    } catch (error) {
+        console.error("❌ Lỗi:", error.message);
+        process.exit(1);
+    }
+}
+
+updateRSS();
